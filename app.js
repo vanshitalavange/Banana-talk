d83226ea355e6517c7e100e1682b8465daf2cff5
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-26 20:42:01 +0530

    input processing output done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var textInput = document.querySelector("#txt-input");
-
+var outputDiv = document.querySelector("#output");
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked");
-    console.log(textInput.value);
+   outputDiv.innerText = "sjmsb" + textInput.value;
 })
 
