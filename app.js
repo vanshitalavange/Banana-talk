84133cb3f97f81e7f8a9630f75f3473ad41fb4da
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-27 02:23:59 +0530

    minor fix

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-
-//var serverURL =  "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
