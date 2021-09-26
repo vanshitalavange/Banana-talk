903a7e641ab1672bf16f3f4a71027a5a20da35da
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-26 22:30:39 +0530

    html and js done

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-var btnTranslate = document.querySelector('#btn-translate');
+var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-btnTranslate.addEventListener("click",function clickEventHandler(){
-   outputDiv.innerText = "sjmsb" + textInput.value;
-})
 
+//var serverURL =  "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Server is down. Try again after some time");
+}
+function clickHandler() {
+  var inputText = textInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
